20707b432341bf5b1c3bf350c3c3f9bf25a40047
szambranod-beep <szambranod@sanmateo.edu.co>
2026-05-03 21:05:02 -0500

    >:c

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,23 +1,16 @@
-const sessionsService = require("../services/sessions.service");
-
 module.exports = async (req, res, next) => {
-  try {
-    const token = req.headers["x-session-token"];
+  const token = req.headers["x-session-token"];
 
-    if (!token) {
-      return res.status(401).json({ message: "Token requerido" });
-    }
-
-    const session = await sessionsService.getByToken(token);
-
-    if (!session) {
-      return res.status(401).json({ message: "Sesión inválida" });
-    }
-
-    req.user = session;
-    next();
-
-  } catch (error) {
-    next(error); 
+  if (!token) {
+    return res.status(401).json({ message: "Token requerido" });
   }
+
+  const session = await sessionsService.getByToken(token);
+
+  if (!session) {
+    return res.status(401).json({ message: "Sesión inválida" });
+  }
+
+  req.user = session;
+  next();
 };
